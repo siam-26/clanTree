5ee8f09518a6cbe52acfe32669e6c0c487271f4b
siam-26 <siamshahriar390@gmail.com>
2023-03-08 15:22:56 +0600

    practice 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Ore amar desh Bangladesh</h1>
+      <h1>Ore amar sonar desh Bangladesh</h1>
     </div>
   );
 }
